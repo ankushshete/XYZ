fb4f72d9f80a0d8e2f98e7c31ca5aae77339371e
ankushshete <ankushshete007@gmail.com>
2024-06-07 17:41:16 +0530

    added code for subtraction

--- arithmatic.py
+++ arithmatic.py
@@ -2,7 +2,15 @@ def add(num1, num2):
     ans = num1 + num2
     return ans
 
+
+def sub(num1, num2):
+    ans = num1 - num2
+    return ans
+
+
 a = 10
 b = 20
-ans = add(a,b)
+ans = add(a, b)
 print(f"Addition of {a} and {b} is {ans}")
+ans = sub(a, b)
+print(f"subtraction of {a} and {b} is {ans}")
